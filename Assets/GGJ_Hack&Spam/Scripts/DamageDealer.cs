0957b33431866dcbe6d4b7d9fc5a79530df42b2b
Aridias <thomas.chiroussot@gmail.com>
2016-01-30 13:38:08 +0100

    Menu ingame

--- Assets/GGJ_Hack&Spam/Scripts/DamageDealer.cs
+++ Assets/GGJ_Hack&Spam/Scripts/DamageDealer.cs
@@ -6,23 +6,34 @@ public class DamageDealer : MonoBehaviour {
 	public	int	damage;
 	public	bool	hero;
 	public	bool	other;
+	public  float force = 5f;
+	private Player _player;
+	private Monster _monster;
 
 	// Use this for initialization
 	void Start () {
-	
+		if (hero)
+			_player = transform.parent.GetComponent<Player> ();
+		else
+			_monster = transform.parent.GetComponent<Monster> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (hero && coll.gameObject.GetComponent<Monster>()) {
 			coll.gameObject.SendMessage("OnDamaged", damage);
+			if (_player.transform.position.x < coll.transform.position.x)
+				coll.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(force, force);
+			else
+				coll.gameObject.GetComponent<Rigidbody2D>().velocity =  new Vector2(-force, force);
 		}
 		else if (other && coll.gameObject.GetComponent<Player>()) {
 			coll.gameObject.SendMessage("OnDamaged", damage);
+			if (_monster.transform.position.x < coll.transform.position.x)
+				coll.gameObject.GetComponent<Rigidbody2D>().velocity =  new Vector2(force, force / 2);
+			else
+				coll.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-force, force / 2);
+
 		}
 	}
 }
